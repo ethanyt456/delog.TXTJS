a959882944bcb4e7816d18118664b618dbdbf044
ethanyt456 <ethanyou204829@gmail.com>
2020-02-25 20:59:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+    public void();
+
+    console.log(add('text'=>'game_mach');
+    var text = 'hello {input;}username';
+    var delay = 1fs;
+    function(e)
+
+console.log(add('delay' => 'text');
